fa4969cf7dabaa0ed8384d52f0321aa3e24228d1
DrankWhitePilot <DrankWhitePilot@users.noreply.github.com>
2026-06-24 15:11:25 +0000

    Remove duplicate loader call

--- homework_07/src/mission_processor.cpp
+++ homework_07/src/mission_processor.cpp
@@ -25,11 +25,6 @@ int MissionProcessor::init()
         return 1;
     }
 
-    if (loader_->load() != 0)
-    {
-        return 1;
-    }
-
     prepareInputData();
     reset();
     initialized_ = true;
